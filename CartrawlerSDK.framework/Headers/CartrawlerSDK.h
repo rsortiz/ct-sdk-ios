1a47f1f7a0f3542e50f4fdff537adf6fe15974e7
Rafael Ortiz <rortiz@cartrawler.com>
2020-02-25 11:27:43 +0000

    Release 0.0.3

---
+++ CartrawlerSDK.framework/Headers/CartrawlerSDK.h
@@ -0,0 +1,19 @@
+//
+//  CartrawlerSDK.h
+//  CartrawlerSDK
+//
+//  Created by Rafael Ortiz on 24/02/2020.
+//  Copyright © 2020 CarTrawler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CartrawlerSDK.
+FOUNDATION_EXPORT double CartrawlerSDKVersionNumber;
+
+//! Project version string for CartrawlerSDK.
+FOUNDATION_EXPORT const unsigned char CartrawlerSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CartrawlerSDK/PublicHeader.h>
+
+
