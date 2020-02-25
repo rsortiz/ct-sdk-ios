b689b7090d03a5f6d1999478a277b7264f2fc8de
Rafael Ortiz <rortiz@cartrawler.com>
2020-02-25 13:48:24 +0000

    Release 0.0.6

--- CTSDKCore.framework/Headers/CTSDKCore.h
+++ CTSDKCore.framework/Headers/CTSDKCore.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char CTSDKCoreVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CTSDKCore/PublicHeader.h>
 
-
+#import "CTSDKCore-Swift.h"
